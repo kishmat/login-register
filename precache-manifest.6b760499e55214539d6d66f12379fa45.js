9664a72f5b61774f7a25a66bdf996c99360a6ec1
kishmat <kishmat17@gmail.com>
2020-08-16 21:52:11 +0545

    Updates

---
+++ precache-manifest.6b760499e55214539d6d66f12379fa45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "484828dd401562c0874865bd4afb70ed",
+    "url": "/login-register/index.html"
+  },
+  {
+    "revision": "ee1a3f0fd875e00a3800",
+    "url": "/login-register/static/css/main.f0c6f038.chunk.css"
+  },
+  {
+    "revision": "582070d1a438c67f52d4",
+    "url": "/login-register/static/js/2.adb2bef0.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/login-register/static/js/2.adb2bef0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee1a3f0fd875e00a3800",
+    "url": "/login-register/static/js/main.2246e743.chunk.js"
+  },
+  {
+    "revision": "cbfe645657994a03ca58",
+    "url": "/login-register/static/js/runtime-main.2d574e0e.js"
+  }
+]);
